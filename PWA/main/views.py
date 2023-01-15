0bc3fc98c64eec66a61990d4be11e61473291ebb
rsk9607 <108059528+rsk9607@users.noreply.github.com>
2023-01-15 11:10:33 +0530

    rendering html from temp

--- PWA/main/views.py
+++ PWA/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+
+def main(request):
+    template = loader.get_template('menu.html')
+    return HttpResponse(template.render())
