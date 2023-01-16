20967ddbe53a83f25f964066ba7fd5642220cf5a
rsk9607 <108059528+rsk9607@users.noreply.github.com>
2023-01-16 15:50:35 +0530

    bugsssssssssssssssssss

--- nigthcanteen/PWA/main/views.py
+++ nigthcanteen/PWA/main/views.py
@@ -5,8 +5,8 @@ from .models import menu
 
 # Create your views here.
 
-def night_canteen(request):
-    template = loader.get_template('night_canteen.html')
+def main(request):
+    template = loader.get_template('main.html')
     return HttpResponse(template.render())
 
 def Menu(request):
