58462fb7611817fc712afcf931a9a192170a1050
rsk9607 <108059528+rsk9607@users.noreply.github.com>
2023-01-17 22:03:10 +0530

    install whitenoise

--- nigthcanteen/PWA/PWA/settings.py
+++ nigthcanteen/PWA/PWA/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'PWA.urls'
@@ -120,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'allcssfiles'
 STATIC_URL = 'static/'
 
 # Default primary key field type
